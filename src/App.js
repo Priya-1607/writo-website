2995a75d6fec85a8b95848b7f7b4ae7fe67b9ff9
Priya-1607 <91568553+Priya-1607@users.noreply.github.com>
2023-07-31 15:40:16 +0530

    from akshat branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App1.css";
 import React, { useState } from "react";
 import LoginForm from "./LoginForm";
-// import NavBar from "./NavBar";
+
 export default function App() {
   const [seen, setSeen] = useState(false);
 
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -10,7 +10,7 @@ function LoginForm(props) {
   const toggleMode = () => {
     setIsSignUpMode(!isSignUpMode);
   };
-//new change
+
 
   return (
     <div className="popup">
